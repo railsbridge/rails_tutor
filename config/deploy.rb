e719b18ac3b4054b8dc2824f36ec0c0e49390609
Mike Gunderloy <MikeG1@larkfarm.com>
2009-05-04 08:37:33 -0500

    Configure for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,33 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "railstutor"
+set :repository,  "git@papaya.larkware.com:hardscapes.git"
+set :user, "webadmin"
+set :deploy_via, :copy
+set :scm, :git
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+# set :deploy_to, "/Library/Rails/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :web, "72.249.191.152"
+role :app, "72.249.191.152"
+role :db,  "72.249.191.152", :primary => true
+
+set :keep_releases, 6
+after "deploy:update", "deploy:cleanup"
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
